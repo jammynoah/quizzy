d60bcb0819ed1c3b95eb15fe8c6fe9df36924012
durgesh shukla <130758819+jammynoah@users.noreply.github.com>
2024-03-03 02:13:49 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: 'center',
+        marginBottom: 10,
+      }}
+    >
+      Made with ♥ by{' '}
+      <a
+        href="https://www.linkedin.com/in/durgesh-shukla-89ab5526b/"
+        style={{ cursor: 'pointer' }}
+      >
+        Jammy Noah
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
